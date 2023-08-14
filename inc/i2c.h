5ce6ae372c9fc19b150ed16c2201720cc940fbc4
Mateusz Bugaj <mateuszbugaj5@gmail.com>
2023-08-14 19:35:22 +0200

    START and STOP conditions, MASTER and SLAVE roles

--- inc/i2c.h
+++ inc/i2c.h
@@ -1,7 +1,13 @@
 #ifndef I2C_H
 #define I2C_H
 #include <stdbool.h>
+#include <avr/delay.h>
 #include "hal.h"
+
+enum I2C_Role {
+  MASTER,
+  SLAVE
+};
 
 /*
   Logging level:
@@ -15,9 +21,15 @@ typedef struct {
   uint8_t addr;
   bool respondToGeneralCall;
   uint8_t loggingLevel;
+  enum I2C_Role role;
+  HALPin SCLPin;
+  HALPin SDAPin;
 } I2C_Config;
 
 void I2C_init(I2C_Config* config);
-void I2C_setPrintFunc(void (*ptr)(const char[], uint8_t, ...));
+void I2C_setPrintFunc(void (*ptr)(const char[]));
+void I2C_setPrintNumFunc(void (*ptr)(const int));
+void I2C_sendStartCondition(void);
+void I2C_sendStopCondition(void);
 
 #endif /* I2C_H */
--- src/I2C.c
+++ src/I2C.c
@@ -17,22 +17,68 @@ uint8_t DSRCounter = 0;
   Pointer to printing function. Used to print logs for example via USART interface.
   By default, an empty function.
 */
-void defaultPrint(const char chars[], uint8_t count, ...){}
-void (*printFuncPtr)(char content[], uint8_t count, ...) = &defaultPrint;
-void I2C_setPrintFunc(void (*ptr)(const char[], uint8_t, ...)){
-    printFuncPtr = ptr;
-}
+void defaultPrint(const char chars[]){}
+void defaultPrintNum(const int value ){}
+void (*printFuncPtr)(char content[]) = &defaultPrint;
+void (*printNumFuncPtr)(int value) = &defaultPrintNum;
+void I2C_setPrintFunc(void (*ptr)(const char[])){ printFuncPtr = ptr; }
+void I2C_setPrintNumFunc(void (*ptr)(const int)){ printNumFuncPtr = ptr; }
 
 I2C_Config* cfg;
 
-void I2C_init(I2C_Config* config){
-  cfg = config;
-  logging("I2C Start", 0);
+void I2C_log(char content[], uint8_t level) {
+  if (cfg->loggingLevel >= level) {
+    (*printFuncPtr)(content);
+    (*printFuncPtr)("\n\r");
+  }
 }
 
-void logging(char content[], uint8_t level) {
+void I2C_logNum(char name[], int value, uint8_t level) {
   if (cfg->loggingLevel >= level) {
-    (*printFuncPtr)(content, 0);
-    (*printFuncPtr)("\n\r", 0);
+    (*printFuncPtr)(name);
+    (*printFuncPtr)(": ");
+    (*printNumFuncPtr)(value);
+    (*printFuncPtr)("\n\r");
   }
+}
+
+void I2C_init(I2C_Config* config){
+  cfg = config;
+
+  I2C_log("I2C Start", 0);
+  I2C_log(cfg->role==MASTER?"MASTER":"SLAVE", 1);
+  I2C_logNum("Addr", cfg->addr, 1);
+
+  hal_pin_direction(cfg->SCLPin, OUTPUT);
+  hal_pin_direction(cfg->SDAPin, OUTPUT);
+}
+
+void I2C_releasePin(HALPin pin){
+  hal_pin_write(pin, LOW);
+}
+
+void I2C_pullDownPin(HALPin pin){
+  hal_pin_write(pin, HIGH);
+}
+
+void I2C_wait() {
+    _delay_ms(500);
+}
+
+/* Change of SDA HIGH -> LOW with SCL HIGH */
+void I2C_sendStartCondition() {
+  I2C_log("Sending START", 3);
+  I2C_pullDownPin(cfg->SDAPin);
+  I2C_wait();
+  I2C_pullDownPin(cfg->SCLPin);
+  I2C_wait();
+}
+
+/* Change of SDA LOW -> HIGH with SCL HIGH */
+void I2C_sendStopCondition() {
+  I2C_log("Sending STOP", 3);
+  I2C_releasePin(cfg->SCLPin);
+  I2C_wait();
+  I2C_releasePin(cfg->SDAPin);
+  I2C_wait();
 }
